03975e2d782fad556bb07d08c9975b20053b6adb
Sandesh92 <36872123+Sandesh92@users.noreply.github.com>
2020-03-15 14:56:15 +0545

    Create EnhancedLopp.java

---
+++ assignment_3/EnhancedLopp.java
@@ -0,0 +1,17 @@
+class EnhancedLoop{
+
+    public static void main(String[] args){
+
+
+
+        char[] vowels = {'a' , 'e' , 'i' , 'o' , 'u'};
+
+        for( char i: vowels){
+
+            System.out.println(i);
+
+        }
+
+    }
+
+}
